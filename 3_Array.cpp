5beaff6a2fd697834dcd15f71400ffe646810e0b
Lam Tan Trong <116331097+lamtantrong@users.noreply.github.com>
2022-11-29 10:21:37 +0700

    Create 3_Array.cpp

---
+++ 3_Array.cpp
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define MAX 100
+void menu();
+void nhapMang1Chieu(int a[], int& n);
+void xuatMang1Chieu(int a[], int n);
+int tinhTongMang(int a[], int n);
+int main()
+{
+	int arr[MAX], soPhanTu, luaChon;
+	for (;;)
+	{
+		system("cls");//làm sạch (xoá) màn hình
+		menu();
+		printf(">> Nhap lua chon: "); scanf_s("%d", &luaChon);
+		if (luaChon == 0)break;
+		switch (luaChon)
+		{
+		case 1:
+			nhapMang1Chieu(arr, soPhanTu);
+			printf("Mang vua nhap:\n");
+			xuatMang1Chieu(arr, soPhanTu);
+			break;
+		case 2:
+			printf("Mang vua nhap:\n");
+			xuatMang1Chieu(arr, soPhanTu);
+			break;
+		case 3:
+			xuatMang1Chieu(arr, soPhanTu);
+			printf("\nTong mang: %d", tinhTongMang(arr, soPhanTu));
+			break;
+		default:printf("Vui long lua chon lai!\n");
+			break;
+		}
+		_getch();
+	}
+	return 0;
+}
+void menu()
+{
+	printf("0. Thoat chuong trinh\n");
+	printf("1. Nhap mang 1 chieu\n");
+	printf("2. Xuat mang 1 chieu\n");
+	printf("3. Tinh tong cac gia tri trong mang\n");
+}
+void nhapMang1Chieu(int a[], int& n)
+{
+	do {
+		printf(">> Nhap so luong phan tu n (n > 0): "); scanf_s("%d", &n);
+	} while (n <= 0);
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d]= ", i);
+		scanf_s("%d", &a[i]);
+	}
+}
+void xuatMang1Chieu(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%5d", a[i]);
+}
+int tinhTongMang(int a[], int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += a[i];
+	return sum;
+}
