81a59374f56e5c402d927ea2b8eb0466705e62c9
Lam Tan Trong <116331097+lamtantrong@users.noreply.github.com>
2022-11-29 10:16:57 +0700

    Create 000_ChuongTrinh_DangMenuLuaChon.cpp

---
+++ 000_ChuongTrinh_DangMenuLuaChon.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void menu();
+
+int main()
+{
+
+	int luaChon;
+	for (;;)
+	{
+		system("cls");
+		menu();
+		printf(">> Nhap lua chon: "); scanf_s("%d", &luaChon);
+		if (luaChon == 0)break;
+		switch (luaChon)
+		{
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		default:printf("Khong hop le! Vui long lua chon lai!\n");
+			break;
+		}
+		_getch();
+	}
+	return 0;
+}
+void menu()
+{
+	printf("0. Thoat chuong trinh!\n");
+
+}
